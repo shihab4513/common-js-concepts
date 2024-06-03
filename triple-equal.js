85f5ae046415a5330a5eac087e10ce418a63377c
shihab4513 <shihab4513@gmail.com>
2024-06-03 07:46:35 +0600

    triple-equal.js added .We should alsways use trple equal this is the main thing of this file

---
+++ triple-equal.js
@@ -0,0 +1,13 @@
+// always use triple equal
+/*
+double equal(==) just check values where as triple equal check type and value of the data
+double equal jodi same type er hoi tahole value check korbe r type different type hole type ta ke casting kore same kore anar try korbe then ckeck korbe
+*/
+const first = 2, second = '2';
+if (first === second) {
+    console.log('values are equal');
+}
+else {
+    console.log('values are not equal');
+}
+
