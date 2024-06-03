d99842327b7e608b6bb1768e301588297f72ebe1
shihab4513 <shihab4513@gmail.com>
2024-06-03 13:14:16 +0600

    closure.js . here how stopwatch work its shown using clousure. Its a very tricky topic and must watch!!

---
+++ closure.js
@@ -0,0 +1,24 @@
+function kitchen() {
+    let roast = 0;
+    return function () {
+        roast++;
+        return roast;
+    }
+}
+// Here below firseServer variable it is returning anonymous function 
+const firstServer = kitchen();
+// Below each time firstServer is calling anonymous function is running. since roast is out of anonymous function inside anonymous function it is working. So each time value of roast increasing by 1 
+console.log(firstServer());
+console.log(firstServer());
+console.log(firstServer());
+console.log(firstServer());
+console.log(firstServer());
+console.log(firstServer());
+console.log('*************************');
+// But if we create new variable then it will be a new instance it will start with clean slate. 
+const secondServer = kitchen();
+console.log(secondServer());
+console.log(secondServer());
+console.log(secondServer());
+console.log(secondServer());
+console.log(secondServer());
